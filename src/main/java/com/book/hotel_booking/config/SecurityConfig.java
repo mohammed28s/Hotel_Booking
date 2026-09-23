8aec9e29f867edf3a3fa02e5fc1ef1cdb6ee5ab0
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2026-09-23 13:26:16 +0400

    The Spring Security has been updated with new changes for the h2 console

--- src/main/java/com/book/hotel_booking/config/SecurityConfig.java
+++ src/main/java/com/book/hotel_booking/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
