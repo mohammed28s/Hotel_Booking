72cb5ea05bc6ee28ad0474a7af7c8d872e8f42d3
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2026-09-24 11:09:35 +0400

    The User entity has been created

---
+++ src/main/java/com/book/hotel_booking/user/Role.java
@@ -0,0 +1,6 @@
+package com.book.hotel_booking.user;
+
+
+public enum Role {   // this is for the managing the user based on their role
+    CUSTOMER, PROVIDER, ADMIN
+}
---
+++ src/main/java/com/book/hotel_booking/user/User.java
@@ -0,0 +1,39 @@
+package com.book.hotel_booking.user;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {  // User info attributes
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled = true;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now(); // Getting the UTC based on the location you are in
+
+
+}
